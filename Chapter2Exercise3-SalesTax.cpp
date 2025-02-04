b162c25f3c9576e1ff037b72eb543bc46495e323
Morgan Thorne <mt34452@email.vccs.edu>
2025-02-03 23:24:12 -0500

    Allowed user to enter amount purchased and the sales tax. Removed state tax and county tax.

--- Chapter2Exercise3-SalesTax.cpp
+++ Chapter2Exercise3-SalesTax.cpp
@@ -5,6 +5,11 @@ Date: February 2025
 Goal:
 Write a program that will compute the total sales tax on a $95 purchase. 
 Assume the state sales tax is 4 percent, and the county sales tax is 2 percent.
+
+Revisions:
+instead of having state and country sales tax hard-coded in your program allow
+the user to enter the sales taxes as well as the amount of the purchase and display
+the total Sales tax.
 */
 
 #include <iostream>
@@ -17,11 +22,35 @@ int main()
     
     float purchase;
     
+    float salestax;
+    
     cout << "What was the total price of all the items bought by the customer? $";
     cin >> purchase;
+    /*I'm not completely sure how to stop the code repeating the purchase after entering the price*/
     cout << fixed << setprecision(2) << purchase << endl;
+    cout << "What is the sales tax? (place percentage as decimal) ";
+    cin >> salestax;
+
+    float salestaxamount = (purchase * salestax);
+
+    float sum = (salestaxamount + purchase);
+
+    cout << "The total amount with the purchase and sales tax is " << sum << endl;
+
+    cout << "   Reciept:" << endl;
+    cout << "   Item Purchased: $" << purchase << endl;
+    cout << "   Sales Tax: $" << salestaxamount << endl;
+    cout << "   -------------------" << endl;
+    cout << "   Total: $" << sum << endl;
 
 
+
+
+    
+
+
+    /*
+    Original Code
     float statetax = 0.04;
     
     float countytax = 0.02;
@@ -41,6 +70,6 @@ int main()
     cout << "   County tax: $" << countytaxvalue << endl;
     cout << "   -------------------" << endl;
     cout << "   Total: $" << sum << endl;
-
+    */
 }
 
